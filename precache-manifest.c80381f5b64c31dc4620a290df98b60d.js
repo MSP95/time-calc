a71b1c75c39645154571ad60c97a33dd14b0c2ef
MSP95 <patilmanish1910@gmail.com>
2019-10-02 17:28:22 -0400

    Updates

---
+++ precache-manifest.c80381f5b64c31dc4620a290df98b60d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa56165c14d2cee7f0f08034f2289b2f",
+    "url": "/time-calc/index.html"
+  },
+  {
+    "revision": "ffdba21454fe86e14b86",
+    "url": "/time-calc/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c6d884b06bbbb6937693",
+    "url": "/time-calc/static/js/2.81db6b96.chunk.js"
+  },
+  {
+    "revision": "ffdba21454fe86e14b86",
+    "url": "/time-calc/static/js/main.1321ab09.chunk.js"
+  },
+  {
+    "revision": "1b29ad9447bd0a17a775",
+    "url": "/time-calc/static/js/runtime-main.f39fc639.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/time-calc/static/media/logo.25bf045c.svg"
+  }
+]);
